8a5e9be43e1d91ba4b9e3beae57f6e8edb4a87e0
prpatel <pratik@mypatelspace.com>
2019-12-04 07:39:33 -0500

    put db script in wrong place and branch DOH

--- exercise4/start-local-test-db.sh
+++
@@ -1,4 +0,0 @@
-docker run --ulimit memlock=-1:-1 -it --rm=true --memory-swappiness=0 \
-    --name postgres-quarkus-example -e POSTGRES_USER=quarkus \
-    -e POSTGRES_PASSWORD=quarkus -e POSTGRES_DB=quarkus \
-    -p 5432:5432 postgres:10.5
